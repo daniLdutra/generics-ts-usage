e9e57b2edf9d095dcdf6fcf388b268cd1490207f
Daniella Dutra <dani.ldutra@gmail.com>
2024-01-07 11:21:03 -0300

    feat: use case in functions

---
+++ index.ts
@@ -0,0 +1,14 @@
+const tasks = [1, 2, 2, 3, 5, 4, 9, 3, 8];
+
+// export function removeDuplicates<T>(tasks: T[]): T[] {
+//   return tasks.filter((item, index) => tasks.indexOf(item) === index);
+// }
+
+//or
+
+export function removeDuplicates<T>(tasks: T[]): T[] {
+  const uniqueSet = new Set<T>(tasks);
+  return Array.from(uniqueSet);
+}
+
+console.log(removeDuplicates(tasks));
